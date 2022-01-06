07d881bc6f0596abec0a70d0df2b61fafab1cbb4
Andrey <azenin010@gmail.com>
2022-01-06 19:31:01 +0400

    2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[]args){
-        
+        int initialAmount = 50;
+        int replenishment = 1500;
+        int bonus = replenishment/100;
+        if(1000 < replenishment);
+        int total = replenishment+initialAmount+bonus;
+        System.out.println("Счет:" + total);
+        System.out.println("Бонусные рубли:" + bonus);
+
+
     }
 }
