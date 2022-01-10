b130c4f8a8187f14b5649e3b7543460c67632383
Andrey <azenin010@gmail.com>
2022-01-10 18:34:20 +0400

    2.2

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
         int initialAmount = 50;
         int replenishment = 1500;
         int bonus = replenishment / 100;
-        {
-            if (replenishment > 1000) {
-            } else {
-            }
-        } ;
         int total = replenishment + initialAmount + bonus;
         System.out.println("Счет:" + total);
         System.out.println("Бонусные рубли:" + bonus);
