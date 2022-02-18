b3fb0c588e9db36641dbb6f5f6c28c26adebb26b
Andrey <azenin010@gmail.com>
2022-02-18 17:45:28 +0400

    2.2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int initialAmount = 100;
-        int replenishment = 1100;
-        int bonus = replenishment / 100;
-        int total = replenishment + initialAmount + bonus;
-        System.out.println("Счет:" + total);
-        System.out.println("Бонусные рубли:" + bonus);
+        int score = 100;
+        int deposit = 1100;
+        int amount = score + deposit;
+
+        int bonus;
+        int finish;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+            finish = amount + bonus;
+            System.out.println("Ваш бонусный счет: " + bonus + " рубль(ей)");
+            System.out.println("Ваш счет: " + finish + " рубль(ей)");
+        } else {
+            System.out.println("Ваш счет: " + amount + " рубль(ей)");
+        }
     }
 }
 
